38da97ba5cd61563ddbe52381cd630cfaba7772c
sebastien1909 <sebastienconfrere6@gmail.com>
2026-03-12 03:14:23 +0100

    laissez moi dormir ah zebi
    modification du server.js, implémentation d'une grosse partie des produits dans la bdd (manque encore les images), catalogue produit fonctionnel, page produit à revoir, mais un début est fait, connexion fonctionnelle, déconnexion fonctionnelle, et plein d'autres trucs un peu plus lambda

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "",
-  database: "Magan",
+  database: "magan_db",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
