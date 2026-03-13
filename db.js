c77304149397523280e9bdbe6dace619944056f6
sebastien1909 <sebastienconfrere6@gmail.com>
2026-03-13 14:55:47 +0100

    update_serveur2

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import mysql from "mysql2/promise";
 
 // CrÃ©e un pool de connexions
 const pool = mysql.createPool({
-  host: "localhost",
+  host: "127.0.0.1",
   user: "root",
   password: "Magan_Serveur_DB",
   database: "magan_db",
