6f6738439faed86aaf4992f8a238d8b2e03e41de
sebastien1909 <sebastienconfrere6@gmail.com>
2026-03-16 11:12:35 +0100

    db local fonctionnel

--- db.js
+++ db.js
@@ -1,11 +1,13 @@
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
 
-// CrÃ©e un pool de connexions
+dotenv.config();
+
 const pool = mysql.createPool({
-  host: "127.0.0.1",
-  user: "site_user",
-  password: "Magan_Serveur_DB",
-  database: "magan_db",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
